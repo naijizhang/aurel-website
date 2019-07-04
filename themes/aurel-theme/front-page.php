236e38e185690d570469bc87d3f9dddadec54484
Naiji Zhang <naijizhang@gmail.com>
2019-07-04 16:14:43 -0700

    Add front page

---
+++ themes/aurel-theme/front-page.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content' ); ?>
+
+			<?php endwhile; ?>
+
+	
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>

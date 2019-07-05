3cc256c41e1a548d48bbaefa010e9cefb0e440e6
Naiji Zhang <naijizhang@gmail.com>
2019-07-05 11:44:16 -0700

    Add footer markup and styles

--- themes/aurel-theme/footer.php
+++ themes/aurel-theme/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer.
  *
@@ -7,16 +8,36 @@
 
 ?>
 
-			</div><!-- #content -->
+</div><!-- #content -->
 
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
+<footer id="myFooter" class="site-footer" role="contentinfo">
+	<nav id="footer-navigation" role="navigation">
+		<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
+	</nav><!-- #footer-navigation -->
+	<div id="footer-middle-column">
+		<p class="footer-brief-description">Aurel offers software sales and training and a full line of services, along with custom unit module development. </p>
+		<p class="copyright">&copy;2019 Aurel System Inc.</p>
+	</div>
+	<div id="footer-right-column">
+		<a class="software-link" href="<?php echo get_home_url(); ?>">
+			<img class="software-logo" alt="home logo" src='<?php echo get_template_directory_uri() . '/assets/images/cp(blue3black).bmp'; ?>'>
+		</a>
+		<div class="social-media-container">
+			<a class="link-googlePlus" href="<?php echo esc_url( 'http://www.google.ca' ); ?>">
+				<img class="googleplus-logo" alt="google plus logo" src='<?php echo get_template_directory_uri() . '/assets/icons/googleplus.svg'; ?>'>
+			</a>
+			<a class="link-linkedin"  href="<?php echo esc_url( 'http://www.google.ca' ); ?>">
+				<img class="linkedin-logo" alt="linkedin logo" src='<?php echo get_template_directory_uri() . '/assets/icons/linkedin.svg'; ?>'>
+			</a>
+		</div>
 
-		<?php wp_footer(); ?>
+	</div>
 
-	</body>
+</footer><!-- #myFooter -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
 </html>

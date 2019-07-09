230e6c0cb0f4f992914a1a0d4d9aaa31a53edad3
Naiji Zhang <naijizhang@gmail.com>
2019-07-09 11:31:32 -0700

    Add jquery file

---
+++ themes/aurel-theme/js/animations.js
@@ -0,0 +1,19 @@
+(function($) {
+    $(function() {
+      //front page industry hover
+    //   $('.industry-logo-container').hover( function() {
+    //     $('.Basketball-menu p').addClass('orange-underscore');
+    //     $('.Rugby-menu p').removeClass('orange-underscore');
+    //     $('.Other-menu p').removeClass('orange-underscore');
+    //     $('.Soccer-menu p').removeClass('orange-underscore');
+    //     $('.Volleyball-menu p').removeClass('orange-underscore');
+    //   });
+   
+    });
+    // if (jQuery('body').hasClass('single-university')) {
+    //   $('nav').addClass('half-transparent');
+    // }
+   
+  
+  })(jQuery);
+  

02a64a97868a3e2e254b508d86a54425b9be1aa1
Naiji Zhang <naijizhang@gmail.com>
2019-07-10 16:51:19 -0700

    Add company history page content and animation

--- themes/aurel-theme/js/animations.js
+++ themes/aurel-theme/js/animations.js
@@ -1,6 +1,6 @@
 (function($) {
-    $(function() {
-      //front page industry hover
+  $(function() {
+    //front page industry hover
     //   $('.industry-logo-container').hover( function() {
     //     $('.Basketball-menu p').addClass('orange-underscore');
     //     $('.Rugby-menu p').removeClass('orange-underscore');
@@ -8,18 +8,44 @@
     //     $('.Soccer-menu p').removeClass('orange-underscore');
     //     $('.Volleyball-menu p').removeClass('orange-underscore');
     //   });
-   
+
+    //widget menu toggle
+    $('.widget-area').after("<button class='toggle-widget'>Click</button>");
+    $('.toggle-widget').click(function() {
+      if ($('.widget-area').css('display') !== 'none')
+        $('.widget-area').css('display', 'none');
+      else $('.widget-area').css('display', 'block');
     });
+
+    //company history animation
+    $('.company-history-container p:odd').after("<div class='helper-block in-view'><div class='circle'></div></div>");
+    $('.company-history-container p:even').before("<div class='helper-block in-view'><div class='circle'></div></div>");
+
+
+
+    //start of scroll function.
+    const elemInView = $('.in-view');
+    $(window).scroll(function() {
+      let yPos = $(window).scrollTop();
+
+      $.each(elemInView, function(index, value) {
+        if (yPos > value.offsetTop - 400 && yPos < value.offsetTop - 250) {
+          console.log(value.offsetTop);
+          $(this).addClass('is-active');
+        } else {
+          $(this).removeClass('is-active');
+        }
+      });
+    });
+
     // if (jQuery('body').hasClass('single-university')) {
     //   $('nav').addClass('half-transparent');
     // }
-   
-      if(jQuery('body').hasClass('logged-in')){
+
+    if (jQuery('body').hasClass('logged-in')) {
       $('.site-header').addClass('fixed-header-logged');
-     }else{
+    } else {
       $('.site-header').addClass('fixed-header-unlog');
-     }
-   
-  
-  })(jQuery);
-  
+    }
+  });
+})(jQuery);
---
+++ themes/aurel-theme/page-company-history.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * The template for displaying all pages.
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area content-company-history">
+    <main id="main" class="site-main" role="main">
+
+        <?php while (have_posts()) : the_post(); ?>
+            <?php the_title('<h2>', '</h2>'); ?>
+            <div class="company-history-container">
+                <?php the_content(); ?>
+            </div>
+        <?php endwhile; // End of the loop. 
+        ?>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>

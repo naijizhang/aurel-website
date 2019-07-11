7fd7818d2f436be14fd3d4a9e961831d406e7765
Naiji Zhang <naijizhang@gmail.com>
2019-07-11 16:56:35 -0700

    Add mobile size header styles and animations

--- themes/aurel-theme/js/animations.js
+++ themes/aurel-theme/js/animations.js
@@ -14,7 +14,7 @@
       "<div class='toggle-widget'><button class='toggle-widget-button'><div>HIDE</div></button></div>"
     );
     $('.toggle-widget').click(function() {
-      if ($('.widget-area').css('visibility') !== 'hidden'){
+      if ($('.widget-area').css('visibility') !== 'hidden') {
         $('.widget-area').css('visibility', 'hidden');
         $('.widget-area').css('opacity', '0');
         $('.toggle-widget br').css('display', 'block');
@@ -22,9 +22,8 @@
         $('.toggle-widget-button').css('width', '30px');
         $('.toggle-widget-button').css('margin-left', '0px');
         $('.toggle-widget-button').css('background-color', '#397C46');
-        $('.toggle-widget-button div').html("M<br>E<br>N<br>U");
-      }
-      else {
+        $('.toggle-widget-button div').html('M<br>E<br>N<br>U');
+      } else {
         $('.widget-area').css('visibility', 'visible');
         $('.widget-area').css('opacity', '0.8');
         $('.toggle-widget br').css('display', 'none');
@@ -32,7 +31,7 @@
         $('.toggle-widget-button').css('width', '70px');
         $('.toggle-widget-button').css('margin-left', '5px');
         $('.toggle-widget-button').css('background-color', 'rgb(40, 117, 218)');
-        $('.toggle-widget-button div').html("HIDE");
+        $('.toggle-widget-button div').html('HIDE');
       }
     });
 
@@ -61,6 +60,37 @@
         }
       });
     });
+
+    //hamburger menu
+    let isMenuOn = true;
+    const toggle = () => {
+      isMenuOn = !isMenuOn;
+      if (isMenuOn) {
+        $('.site-content').css('right', '0px');
+        $('.site-header ').css('right', '0px');
+        $('#mobile-menu').css('visibility', 'hidden');
+        $('#mobile-menu').css('opacity', '0');
+      } else {
+        $('.site-content').css('right', '180px');
+        $('.site-header ').css('right', '180px');
+        $('#mobile-menu').css('visibility', 'visible');
+        $('#mobile-menu').css('opacity', '1');
+      }
+    };
+    $('input[type=checkbox]').on('click', function() {
+      toggle();
+    });
+
+    $( window ).resize(function() {
+      if($( window ).width()>720 ){
+        if(!isMenuOn){
+          toggle();
+          $('.nav-trigger').prop('checked', false); // Unchecks it
+        }
+      }
+    });
+   
+
 
     // if (jQuery('body').hasClass('single-university')) {
     //   $('nav').addClass('half-transparent');

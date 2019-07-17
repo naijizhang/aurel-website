7dcd35a0c9b260d327302bcc2ea3e0906560af3c
Naiji Zhang <naijizhang@gmail.com>
2019-07-17 14:45:59 -0700

    Update hero image class name in about page

---
+++ themes/aurel-theme/template-parts/content-withhero.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Template part for displaying page content in withhero.php.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title('<h1 class="hero-title">', '</h1>'); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->

1fe41d672640776410c0ca25ee78d7b194a81c1f
Naiji Zhang <naijizhang@gmail.com>
2019-07-17 16:44:02 -0700

    Add Team member orderby date

--- themes/aurel-theme/inc/extras.php
+++ themes/aurel-theme/inc/extras.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Custom functions that act independently of the theme templates.
  *
@@ -11,20 +12,24 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function red_starter_body_classes( $classes ) {
+function red_starter_body_classes($classes)
+{
 	// Adds a class of group-blog to blogs with more than 1 published author.
-	if ( is_multi_author() ) {
+	if (is_multi_author()) {
 		$classes[] = 'group-blog';
 	}
 
 	return $classes;
 }
-add_filter( 'body_class', 'red_starter_body_classes' );
+add_filter('body_class', 'red_starter_body_classes');
 
 //display the all the team members
 function aurel_get_members()
 {
-	$args = array('post_type' => 'aurel-team', 'posts_per_page' => -1, 'orderby' => 'date');
+	$args = array(
+		'post_type' => 'aurel-team', 'posts_per_page' => -1,   'orderby' => 'date',
+		'order'   => 'ASC',
+	);
 	$members = get_posts($args);
 	return $members;
 }

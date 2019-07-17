4d2ae3bb6d24cce49e872aa646a02c9cc4d446a3
Naiji Zhang <naijizhang@gmail.com>
2019-07-17 12:23:56 -0700

    Add front-page mobile-size styling

--- themes/aurel-theme/front-page.php
+++ themes/aurel-theme/front-page.php
@@ -47,7 +47,7 @@ get_header(); ?>
             <div class="hexagon" data-aos="fade-right">
                 <div class="hexagon-industry-container1">
                     <a class="industry-link" href="<?php echo get_home_url(); ?>">
-                        <div class="industry-logo-container">
+                        <div class="industry-logo-container industry1-container">
                             <img class="industry-logo" alt="industry logo" src='<?php echo get_template_directory_uri() . '/assets/icons/paper.svg'; ?>'>
                             <p class="industry-title">Pulp & Paper</p>
                         </div>
@@ -56,13 +56,13 @@ get_header(); ?>
                 </div>
                 <div class="hexagon-industry-container2">
                     <a class="industry-link" href="<?php echo get_home_url(); ?>">
-                        <div class="industry-logo-container">
+                        <div class="industry-logo-container industry2-container">
                             <img class="industry-logo" alt="industry logo" src='<?php echo get_template_directory_uri() . '/assets/icons/rose.svg'; ?>'>
                             <p class="industry-title">Forest Products</p>
                         </div>
                     </a>
                     <a class="industry-link" href="<?php echo get_home_url(); ?>">
-                        <div class="industry-logo-container">
+                        <div class="industry-logo-container industry3-container">
                             <img class="industry-logo" alt="industry logo" src='<?php echo get_template_directory_uri() . '/assets/icons/pizza.svg'; ?>'>
                             <p class="industry-title">Food Processing</p>
                         </div>
@@ -70,13 +70,13 @@ get_header(); ?>
                 </div>
                 <div class="hexagon-industry-container3">
                     <a class="industry-link" href="<?php echo get_home_url(); ?>">
-                        <div class="industry-logo-container">
+                        <div class="industry-logo-container industry4-container">
                             <img class="industry-logo" alt="industry logo" src='<?php echo get_template_directory_uri() . '/assets/icons/water.svg'; ?>'>
                             <p class="industry-title">Water & Waste Water</p>
                         </div>
                     </a>
                     <a class="industry-link" href="<?php echo get_home_url(); ?>">
-                        <div class="industry-logo-container">
+                        <div class="industry-logo-container industry5-container">
                             <img class="industry-logo" alt="industry logo" src='<?php echo get_template_directory_uri() . '/assets/icons/hammer.svg'; ?>'>
                             <p class="industry-title">Mineral Processing</p>
                         </div>
@@ -84,7 +84,7 @@ get_header(); ?>
                 </div>
                 <div class="hexagon-industry-container4">
                     <a class="industry-link" href="<?php echo get_home_url(); ?>">
-                        <div class="industry-logo-container">
+                        <div class="industry-logo-container industry6-container">
                             <img class="industry-logo" alt="industry logo" src='<?php echo get_template_directory_uri() . '/assets/icons/book.svg'; ?>'>
                             <p class="industry-title">Research & learning</p>
                         </div>
@@ -110,14 +110,14 @@ get_header(); ?>
         <div class="front-page-bottom-banner">
             <div class="row-first">
                 <a>
-                    <div data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000" class="polygon-container">
+                    <div data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000" class="polygon-container" id="polygon-block1">
                         <h3>CADSIM Plus</h3>
                         <h3>software</h3>
                     </div>
                 </a>
                 <img class="cadsim-logo" alt="cadsim logo" src='<?php echo get_template_directory_uri() . '/assets/images/cp(blue3black).bmp'; ?>'>
                 <a>
-                    <div data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1500" class="polygon-container">
+                    <div data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1500" class="polygon-container" id="polygon-block2">
                         <h3>CADSIM Plus</h3>
                         <h3>training</h3>
                     </div>
@@ -126,13 +126,13 @@ get_header(); ?>
             </div>
             <div class="row-second">
                 <a>
-                    <div data-aos="flip-left" data-aos-easing="ease-out-cub ic" data-aos-duration="1500" class="polygon-container">
+                    <div data-aos="flip-left" data-aos-easing="ease-out-cub ic" data-aos-duration="1500" class="polygon-container" id="polygon-block3">
                         <h3>Moduling</h3>
                         <h3>services</h3>
                     </div>
                 </a>
                 <a>
-                    <div data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000" class="polygon-container">
+                    <div data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000" class="polygon-container" id="polygon-block4">
                         <h3>Online</h3>
                         <h3>application</h3>
                     </div>

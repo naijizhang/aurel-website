6840c4669c2d5eb05556c1d4d6328add514898b4
Naiji Zhang <naijizhang@gmail.com>
2019-07-17 14:55:49 -0700

    Add aurel-team post type

--- plugins/aurel-functionality/lib/functions/post-types.php
+++ plugins/aurel-functionality/lib/functions/post-types.php
@@ -6,3 +6,59 @@
  */
 
 // Add your custom post types here...
+// Register Custom Post Type
+function aurel_team() {
+
+	$labels = array(
+		'name'                  => _x( 'Team Members', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Team Member', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Aurel Team', 'text_domain' ),
+		'name_admin_bar'        => __( 'Aurel Team', 'text_domain' ),
+		'archives'              => __( 'Aurel Team Archives', 'text_domain' ),
+		'attributes'            => __( 'Aurel Team Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+		'all_items'             => __( 'All Members', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Member', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Member', 'text_domain' ),
+		'edit_item'             => __( 'Edit Member', 'text_domain' ),
+		'update_item'           => __( 'Update Member', 'text_domain' ),
+		'view_item'             => __( 'View Member', 'text_domain' ),
+		'view_items'            => __( 'View Members', 'text_domain' ),
+		'search_items'          => __( 'Search Member', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Profile Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set Profile image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove profile image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as profile image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into member', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this member', 'text_domain' ),
+		'items_list'            => __( 'Member list', 'text_domain' ),
+		'items_list_navigation' => __( 'Members list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter member list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Team Member', 'text_domain' ),
+		'description'           => __( 'aurel team', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-id',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+		'show_in_rest'          => true,
+	);
+	register_post_type( 'aurel-team', $args );
+
+}
+add_action( 'init', 'aurel_team', 0 );

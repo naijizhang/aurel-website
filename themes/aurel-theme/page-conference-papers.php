6c3a7850648060da256d9a5504dd353bfca2cdb4
Naiji Zhang <naijizhang@gmail.com>
2019-07-19 16:31:08 -0700

    Add conference markup and styles

---
+++ themes/aurel-theme/page-conference-papers.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area content-conference">
+    <main id="main" class="site-main" role="main">
+
+        <?php while (have_posts()) : the_post(); ?>
+            <article id="team-top-banner-container" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+                </header><!-- .entry-header -->
+                <div class="entry-content">
+                    <?php the_content(); ?>
+                </div><!-- .entry-content -->
+            </article><!-- #post-## -->
+        <?php endwhile; // End of the loop. 
+        ?>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>

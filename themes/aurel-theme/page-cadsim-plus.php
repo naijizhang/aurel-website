06e1ad62ff0d45e91cf0167367bbcf9d377be20d
Naiji Zhang <naijizhang@gmail.com>
2019-07-28 22:08:32 -0700

    Add cadsim-plus page styles

--- themes/aurel-theme/page-cadsim-plus.php
+++ themes/aurel-theme/page-cadsim-plus.php
@@ -8,7 +8,7 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area newsletters-content">
+<div id="primary" class="content-area cadsim-content">
     <main id="main" class="site-main" role="main">
 
         <?php while (have_posts()) : the_post(); ?>

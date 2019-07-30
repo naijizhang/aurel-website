ea311bcbac6a1d7ed2b521be21abad8281b56af4
Naiji Zhang <naijizhang@gmail.com>
2019-07-30 10:48:26 -0700

    Add contact info block in page template

--- themes/aurel-theme/page.php
+++ themes/aurel-theme/page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages.
  *
@@ -7,17 +8,28 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+		<?php while (have_posts()) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content-page' ); ?>
+			<?php get_template_part('template-parts/content-page'); ?>
 
-			<?php endwhile; // End of the loop. ?>
+		<?php endwhile; // End of the loop. 
+		?>
+		<div class="contact-block">
+			<div class="column">
+				<h3>What can <?php echo the_title();?> do for you?</h3>
+				<p>We are happy to provide more information!</p>
+				<p>Click to e-mail or call us at 604.299.7707</p>
+			</div>
+			<a href="mailto:mail@aurelsystems.com?subject=*Get%20more%20info%20about%20CADSIM%20Plus">
+				<img class="cheatbox-logo" alt="cheatbox logo" src='<?php echo get_template_directory_uri() . '/assets/icons/chatboxes.svg'; ?>'>
+				Get more info...
+			</a>
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-	<?php get_sidebar(); ?>
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>

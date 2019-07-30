c5d2e810c4a1050b6578aa62340d361cb96ce6e1
Naiji Zhang <naijizhang@gmail.com>
2019-07-30 15:57:57 -0700

    Remove contact model from page template

--- themes/aurel-theme/page.php
+++ themes/aurel-theme/page.php
@@ -17,17 +17,6 @@ get_header(); ?>
 
 		<?php endwhile; // End of the loop. 
 		?>
-		<div class="contact-block">
-			<div class="column">
-				<h3>What can <?php echo the_title();?> do for you?</h3>
-				<p>We are happy to provide more information!</p>
-				<p>Click to e-mail or call us at 604.299.7707</p>
-			</div>
-			<a href="mailto:mail@aurelsystems.com?subject=*Get%20more%20info%20about%20CADSIM%20Plus">
-				<img class="cheatbox-logo" alt="cheatbox logo" src='<?php echo get_template_directory_uri() . '/assets/icons/chatboxes.svg'; ?>'>
-				Get more info...
-			</a>
-		</div>
 	</main><!-- #main -->
 </div><!-- #primary -->
 

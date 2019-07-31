528426ac6fadd55a56da2e1647a8a750accff8ab
Naiji Zhang <naijizhang@gmail.com>
2019-07-31 16:53:39 -0700

    Add version history post type and taxonomy

--- plugins/aurel-functionality/lib/functions/post-types.php
+++ plugins/aurel-functionality/lib/functions/post-types.php
@@ -119,3 +119,60 @@ function newsletters() {
 
 }
 add_action( 'init', 'newsletters', 0 );
+
+// Register Custom Post Type
+function version_history() {
+
+	$labels = array(
+		'name'                  => _x( 'Versions', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Version', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Version History', 'text_domain' ),
+		'name_admin_bar'        => __( 'Version History', 'text_domain' ),
+		'archives'              => __( 'Version Archives', 'text_domain' ),
+		'attributes'            => __( 'Version Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Version:', 'text_domain' ),
+		'all_items'             => __( 'All Versions', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Version', 'text_domain' ),
+		'add_new'               => __( 'Add New Version', 'text_domain' ),
+		'new_item'              => __( 'New Version', 'text_domain' ),
+		'edit_item'             => __( 'Edit Version', 'text_domain' ),
+		'update_item'           => __( 'Update Version', 'text_domain' ),
+		'view_item'             => __( 'View Version', 'text_domain' ),
+		'view_items'            => __( 'View Versions', 'text_domain' ),
+		'search_items'          => __( 'Search Version', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into version', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this version', 'text_domain' ),
+		'items_list'            => __( 'Versions list', 'text_domain' ),
+		'items_list_navigation' => __( 'Versions list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter versions list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Version', 'text_domain' ),
+		'description'           => __( 'This is the version history of the software.', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-rest-api',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+		'show_in_rest'          => true,
+	);
+	register_post_type( 'version_history', $args );
+
+}
+add_action( 'init', 'version_history', 0 );
--- plugins/aurel-functionality/lib/functions/taxonomies.php
+++ plugins/aurel-functionality/lib/functions/taxonomies.php
@@ -6,3 +6,42 @@
  */
 
 // Add your custom taxonomies here...
+// Register Custom Taxonomy
+function custom_taxonomy_content_type() {
+
+	$labels = array(
+		'name'                       => _x( 'Content types', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Content type', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Content Type', 'text_domain' ),
+		'all_items'                  => __( 'All Content Types', 'text_domain' ),
+		'parent_item'                => __( 'Parent Content Type', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Content Type:', 'text_domain' ),
+		'new_item_name'              => __( 'Content Type Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add a Content Type', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Content Type', 'text_domain' ),
+		'update_item'                => __( 'Update Content Type', 'text_domain' ),
+		'view_item'                  => __( 'View Content Type', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate Content Types with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove Content Types', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Content Types', 'text_domain' ),
+		'search_items'               => __( 'Search Content Types', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No items', 'text_domain' ),
+		'items_list'                 => __( 'Features list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Features list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+		'show_in_rest'               => true,
+	);
+	register_taxonomy( 'content_type', array( 'version_history' ), $args );
+
+}
+add_action( 'init', 'custom_taxonomy_content_type', 0 );

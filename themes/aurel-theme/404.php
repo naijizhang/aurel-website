669bd43df73a351f157681eae9cf9f8a2a190200
Naiji Zhang <naijizhang@gmail.com>
2019-08-21 14:31:42 -0700

    Add 404 page styles

--- themes/aurel-theme/404.php
+++ themes/aurel-theme/404.php
@@ -9,19 +9,13 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area content-404">
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
 				</header><!-- .page-header -->
-
-				<div class="page-content">
-					
-
-				
-				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->

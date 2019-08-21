e008368798b41d0cfccbafdaae26c62756f10b22
Naiji Zhang <naijizhang@gmail.com>
2019-08-21 14:35:27 -0700

    Add flask in 404 page

--- themes/aurel-theme/404.php
+++ themes/aurel-theme/404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying 404 pages (not found).
  *
@@ -9,16 +10,40 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area content-404">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area content-404">
+	<main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php echo esc_html( 'Oops! That page can&rsquo;t be found.' ); ?></h1>
-				</header><!-- .page-header -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		<section class="error-404 not-found">
+			<header class="page-header">
+				<h1 class="page-title"><?php echo esc_html('Oops! That page can&rsquo;t be found.'); ?></h1>
+			</header><!-- .page-header -->
+		</section><!-- .error-404 -->
+		<div id="flask">
+			<div class="background"></div>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 113 130">
+				<g>
+					<path fill="#fff" d="M0,0v130h112.084L111.75,0H0zM94.75,128C71,128,56,128,56,128s-14.873,0-38.623,0s-13.945-19.422-6.33-35.945S40,41.25,40,41.25V3h16h11v38.25c0,0,23.901,34.283,31.517,50.805S118.5,128,94.75,128z"></path>
+					<path fill="none" stroke="#bbb" stroke-width="5" stroke-miterlimit="10" d="M56,127.5c0,0-14.873,0-38.623,0s-13.695-19.172-6.08-35.695C18.912,75.283,40.5,41.25,40.5,41.25V2.5h-9H56h19.5h-8v38.75c0,0,23.651,34.033,31.267,50.555c7.615,16.523,19.733,35.695-4.017,35.695S56,127.5,56,127.5z"></path>
+				</g>
+			</svg>
+			<div class="bubble b0"></div>
+			<div class="bubble b1"></div>
+			<div class="bubble b2"></div>
+			<div class="bubble b3"></div>
+			<div class="bubble b4"></div>
+			<div class="bubble b5"></div>
+			<div class="bubble b6"></div>
+			<div class="bubble b7"></div>
+			<div class="bubble b8"></div>
+			<div class="bubble b9"></div>
+			<div class="swirl s0"></div>
+			<div class="swirl s1"></div>
+			<div class="swirl s2"></div>
+			<div class="swirl s3"></div>
+			<div class="swirl s4"></div>
+			<div class="swirl s5"></div>
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>

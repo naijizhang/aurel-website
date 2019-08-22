1e3bfea647a8ae42a7fa36cbe221988544490f89
Naiji Zhang <naijizhang@gmail.com>
2019-08-22 16:45:15 -0700

    Remove about page hero image

--- themes/aurel-theme/page-about.php
+++ themes/aurel-theme/page-about.php
@@ -8,13 +8,13 @@
 
 get_header(); ?>
 <?php get_sidebar(); ?>
-<div id="primary" class="content-area content-about-page">
+<div id="primary" class="content-area content-about-page content-all-page">
 
 	<main id="main" class="site-main" role="main">
 
 		<?php while (have_posts()) : the_post(); ?>
 
-			<?php get_template_part('template-parts/content-withhero'); ?>
+			<?php get_template_part('template-parts/content-page'); ?>
 
 		<?php endwhile; // End of the loop. 
 		?>

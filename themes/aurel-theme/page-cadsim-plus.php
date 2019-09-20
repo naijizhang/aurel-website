a0aedade16315ece8954cf1377c899ed3303f87a
Naiji Zhang <naijizhang@gmail.com>
2019-09-20 14:46:10 -0700

    Add modal on cadsim runtime page and fixed errors

--- themes/aurel-theme/page-cadsim-plus.php
+++ themes/aurel-theme/page-cadsim-plus.php
@@ -101,7 +101,7 @@
                         </div>
                     </div>
                     <div class="modal fade" id="quoteModal" tabindex="-1" role="dialog" aria-labelledby="quoteModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-dialog modal-dialog-scrollable" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h5 class="modal-title" id="exampleModalLabel">Get a quote...</h5>
